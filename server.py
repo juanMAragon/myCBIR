e6924be6100775f309a3c90e46fdd3664a5943f5
Juan Aragon <juanm.aramas@gmail.com>
2022-05-24 17:14:53 +0200

    server simulation

---
+++ server.py
@@ -0,0 +1,47 @@
+import numpy as np 
+from PIL import Image 
+from feature_extractor import FeatureExtractor
+from datetime import datetime
+from flask import Flask, request, render_template
+from pathlib import Path 
+import os 
+
+# read image features 
+fe = FeatureExtractor() 
+features = [] 
+img_paths = []
+for feature_path in Path(os.path.join('static', 'feature')).glob('*.npy'):
+    features.append(np.load(feature_path)) 
+    img_paths.append(Path("./static/img") / (feature_path.stem + ".jpg"))
+features = np.array(features) 
+
+
+request_path = os.path.join('static','img', 'a3.jpg')
+img=Image.open(request_path).convert('RGB')
+
+query = fe.extract(img)
+query = query.numpy()
+query = np.expand_dims(query, axis=0)
+
+print('-------------')
+print(type(features))
+print(type(query))
+print('-------------')
+print(features.shape)
+print(query.shape)
+
+
+dists = np.linalg.norm(features-query, axis=2) #L2 distances to features 
+dists = np.squeeze(dists, axis=1)
+
+ids = np.argsort(dists, axis=0)
+
+print('-------------')
+print(dists)
+
+print(ids)
+
+
+scores = [(dists[id], img_paths[id]) for id in ids]
+
+print(scores[:3])
